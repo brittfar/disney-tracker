9299d111347c76acafc815ec54294d4f593507f9
brittfar <dm012486@gmail.com>
2026-01-07 21:55:29 -0600

    Fix start command path and update requirements.txt

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 echo "Starting Scheduler..."
 python scheduler.py &
 echo "Starting Dashboard..."
-streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0
+python -m streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0
