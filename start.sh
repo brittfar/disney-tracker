d31f0da2b5502d9296d313d89a709eedf1b7657c
brittfar <dm012486@gmail.com>
2026-01-13 21:27:06 -0600

    Fix start.sh paths

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Run database initializer (using standard python)
 echo "Initializing Database..."
 python init_db.py
+
+# Start background scheduler
 echo "Starting Scheduler..."
 python scheduler.py &
+
+# Start Dashboard (using standard streamlit)
 echo "Starting Dashboard..."
-python -m streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0
+streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0
